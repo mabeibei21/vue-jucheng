28f08c60f7f4cf3beaab4c1a2f0cc980c448ce6f
BK1917mabeibei <1793408377@qq.com>
2019-11-11 20:33:21 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path=require("path");
+
+module.exports={
+    decServer:{
+        proxy:{
+            "/api":{
+                target:"http://39.97.33.178",
+                changeOrigin:true
+            }
+        }
+    },
+    configureWebpack:{
+        alias:{
+            "@":path.join(__dirname,"./src"),
+            "@api":path.join(__dirname,"./src/api"),
+            "@assets":path.join(__dirname,"./src/assets"),
+            "@common":path.join(__dirname,"./src/common"),
+            "@components":path.join(__dirname,"./src/components"),
+            "@pages":path.join(__dirname,"./src/pages"),
+            "@router":path.join(__dirname,"./src/router"),
+            "@store":path.join(__dirname,"./src/store"),
+            "@utils":path.join(__dirname,"./src/utils"),
+        }
+    }
+}
